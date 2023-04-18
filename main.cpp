979a5d42c4ff7b2f7e526997414819079ddeb721
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-04-18 10:55:07 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@ int main(){
     cin >> length;
     printf("What is the width of the square? ");
     int calc = length*width;
-    cout << "The area is " << calc << "cm";
+    cout << "The area is " << calc << "cm" << endl;
+    main();
   }
   if(area == 2){
     float radius;
     printf("What is the radius of the circle? ");
     cin >> radius;
     float calc = 3.14*radius*2;
-    cout << "The area is " << calc;
+    cout << "The area is " << calc << endl;
+    main();
   }
   if(area == 3){
     float base;
@@ -27,7 +29,8 @@ int main(){
     cin >>> base;
     printf("What is the height? ");
     float calc = base*height/2;
-    cout << "The area is " << calc << "cm/in/ft";
+    cout << "The area is " << calc << "cm/in/ft" << endl;
+    main();
   }
   if(area == 4){
     int amount;
@@ -44,7 +47,8 @@ int main(){
       printf("Enter the third side ");
       cin >> three;
       int result = one+two+three;
-      cout << "The perimeter is " << result + " << cm/in/ft";
+      cout << "The perimeter is " << result + " << cm/in/ft" << endl;
+      main();
     }
     if(amount == 4){
       float one;
@@ -60,7 +64,8 @@ int main(){
       printf("Enter the fourth side ");
       cin >> four;
       int result = one+two+three+four;
-      cout << "The perimeter is " << result << "cm/ft/in";
+      cout << "The perimeter is " << result << " cm/mm/m/ft/in" << endl;
+      main();
     }
     if(amount == 5){
       float one;
@@ -68,6 +73,19 @@ int main(){
       float three;
       float four;
       float five;
+      printf("Enter the first side "); 
+      cin >> one;
+      printf("Enter the second side ");
+      cin >> two;
+      printf("Enter the third side ");
+      cin >> three;
+      printf("Enter the fourth side ");
+      cin >> four;
+      printf("Enter the fifth side ");
+      cin >> five;
+      int result = one+two+three+four+five;
+      cout << "The perimeter is " << result << "cm/mm/m/ft/in" << endl;
+      main();
     }
   }
 }
